8d61cd2c12b2eb897a2d75604c64ea449f6a3d60
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-11-21 17:38:55 +0530

    add header section

---
+++ expense-tracker/app/_components/Header.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+
+function Header() {
+  return (
+    <div className="p-5 flex justify-between items-center border shadow-sm">
+      <Image src={"./logo.svg"} alt="Logo.svg" width={80} height={100} />
+      <Button>Get Started</Button>
+    </div>
+  );
+}
+
+export default Header;
--- expense-tracker/app/page.js
+++ expense-tracker/app/page.js
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
     <div>
-      <Button variant="outline">Hello Welcome</Button>
+      <Header />
     </div>
   );
 }
